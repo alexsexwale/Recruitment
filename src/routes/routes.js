1c0f5383b54279347dd1cc51d651ed27a4edb7ae
DESKTOP-ECPVIEJ\alexs <alex.sexwale@jobox.co.za>
2019-10-08 15:03:25 +0200

    Initial commit

---
+++ src/routes/routes.js
@@ -0,0 +1,214 @@
+import DashboardLayout from "@/pages/Dashboard/Layout/DashboardLayout.vue";
+import AuthLayout from "@/pages/Dashboard/Pages/AuthLayout.vue";
+
+// Dashboard pages
+import Dashboard from "@/pages/Dashboard/Dashboard.vue";
+
+// Pages
+import User from "@/pages/Dashboard/Pages/UserProfile.vue";
+import Lock from "@/pages/Dashboard/Pages/Lock.vue";
+
+// Views
+import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
+import ForgotPassword from "@/views/ForgotPassword.vue";
+
+// Components pages
+import Buttons from "@/pages/Dashboard/Components/Buttons.vue";
+import GridSystem from "@/pages/Dashboard/Components/GridSystem.vue";
+import Panels from "@/pages/Dashboard/Components/Panels.vue";
+import SweetAlert from "@/pages/Dashboard/Components/SweetAlert.vue";
+import Notifications from "@/pages/Dashboard/Components/Notifications.vue";
+import Icons from "@/pages/Dashboard/Components/Icons.vue";
+import Typography from "@/pages/Dashboard/Components/Typography.vue";
+
+// Forms pages
+import RegularForms from "@/pages/Dashboard/Forms/RegularForms.vue";
+import ExtendedForms from "@/pages/Dashboard/Forms/ExtendedForms.vue";
+import ValidationForms from "@/pages/Dashboard/Forms/ValidationForms.vue";
+import Wizard from "@/pages/Dashboard/Forms/Wizard.vue";
+
+// TableList pages
+import RegularTables from "@/pages/Dashboard/Tables/RegularTables.vue";
+import ExtendedTables from "@/pages/Dashboard/Tables/ExtendedTables.vue";
+import PaginatedTables from "@/pages/Dashboard/Tables/PaginatedTables.vue";
+
+// Charts
+import Widgets from "@/pages/Dashboard/Widgets.vue";
+
+let componentsMenu = {
+  path: "/components",
+  component: DashboardLayout,
+  redirect: "/components/buttons",
+  name: "Components",
+  children: [
+    {
+      path: "buttons",
+      name: "Buttons",
+      components: { default: Buttons }
+    },
+    {
+      path: "grid-system",
+      name: "Grid System",
+      components: { default: GridSystem }
+    },
+    {
+      path: "panels",
+      name: "Panels",
+      components: { default: Panels }
+    },
+    {
+      path: "sweet-alert",
+      name: "Sweet Alert",
+      components: { default: SweetAlert }
+    },
+    {
+      path: "notifications",
+      name: "Notifications",
+      components: { default: Notifications }
+    },
+    {
+      path: "icons",
+      name: "Icons",
+      components: { default: Icons }
+    },
+    {
+      path: "typography",
+      name: "Typography",
+      components: { default: Typography }
+    }
+  ]
+};
+let formsMenu = {
+  path: "/forms",
+  component: DashboardLayout,
+  redirect: "/forms/regular",
+  name: "Forms",
+  children: [
+    {
+      path: "regular",
+      name: "Regular Forms",
+      components: { default: RegularForms }
+    },
+    {
+      path: "extended",
+      name: "Extended Forms",
+      components: { default: ExtendedForms }
+    },
+    {
+      path: "validation",
+      name: "Validation Forms",
+      components: { default: ValidationForms }
+    },
+    {
+      path: "wizard",
+      name: "Wizard",
+      components: { default: Wizard }
+    }
+  ]
+};
+
+let tablesMenu = {
+  path: "/table-list",
+  component: DashboardLayout,
+  redirect: "/table-list/regular",
+  name: "Tables",
+  children: [
+    {
+      path: "regular",
+      name: "Regular Tables",
+      components: { default: RegularTables }
+    },
+    {
+      path: "extended",
+      name: "Extended Tables",
+      components: { default: ExtendedTables }
+    },
+    {
+      path: "paginated",
+      name: "Pagianted Tables",
+      components: { default: PaginatedTables }
+    }
+  ]
+};
+
+let mapsMenu = {
+  path: "/maps",
+  component: DashboardLayout,
+  name: "Maps",
+  redirect: "/maps/google"
+};
+
+let pagesMenu = {
+  path: "/pages",
+  component: DashboardLayout,
+  name: "Pages",
+  redirect: "/pages/user",
+  children: [
+    {
+      path: "user",
+      name: "User Page",
+      components: { default: User }
+    }
+  ]
+};
+
+let authPages = {
+  path: "/",
+  component: AuthLayout,
+  name: "Authentication",
+  children: [
+    {
+      path: "/login",
+      name: "Login",
+      component: Login
+    },
+    {
+      path: "/register",
+      name: "Register",
+      component: Register
+    },
+    {
+      path: "/forgot",
+      name: "Forgot",
+      component: ForgotPassword
+    },
+    {
+      path: "/lock",
+      name: "Lock",
+      component: Lock
+    }
+  ]
+};
+
+const routes = [
+  {
+    path: "/",
+    redirect: "/dashboard",
+    name: "Home"
+  },
+  componentsMenu,
+  formsMenu,
+  tablesMenu,
+  mapsMenu,
+  pagesMenu,
+  authPages,
+  {
+    path: "/",
+    component: DashboardLayout,
+    children: [
+      {
+        path: "dashboard",
+        name: "Dashboard",
+        components: { default: Dashboard }
+      },
+      {
+        path: "widgets",
+        name: "Widgets",
+        components: { default: Widgets }
+      }
+    ]
+  }
+];
+
+export default routes;
---
+++ src/views/Register.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="md-layout text-center">
+    <div class="md-layout-item md-size-33 md-medium-size-50 md-small-size-70 md-xsmall-size-100">
+      <login-card header-color="green">
+        <h3 slot="title" class="title">Register</h3>
+        
+        <md-field class="md-form-group" slot="inputs">
+          <md-icon>email</md-icon>
+          <label>Email</label>
+          <md-input v-model="email" type="email"></md-input>
+        </md-field>
+        <md-field class="md-form-group" slot="inputs">
+          <md-icon>lock_outline</md-icon>
+          <label>Password</label>
+          <md-input v-model="password"></md-input>
+        </md-field>
+        <md-field class="md-form-group" slot="inputs">
+          <md-icon>lock_outline</md-icon>
+          <label>Confirm password</label>
+          <md-input v-model="password"></md-input>
+        </md-field>
+        <!-- <md-checkbox v-model="boolean">I agree to the <a>terms and conditions</a>.</md-checkbox> -->
+        <md-button slot="footer" class="md-simple md-success md-lg">
+          Register
+        </md-button>
+      </login-card>
+    </div>
+  </div>
+</template>
+<script>
+import { LoginCard } from "@/components";
+export default {
+  components: {
+    LoginCard
+  },
+  data() {
+    return {
+      firstname: null,
+      email: null,
+      password: null,
+      boolean: false,
+    };
+  }
+};
+</script>
+
+<style></style>
